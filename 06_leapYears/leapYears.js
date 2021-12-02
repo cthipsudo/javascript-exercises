d61e494dfd3b5981439ba6dc4a4b5232b11a4361
cthipsudo <chanpasong.thipphakhinkeo@gmail.com>
2021-12-02 11:13:51 -0600

    Completed JS Exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  // Leap year 
+  // if divisible by 4 and not 100
+  // or divisible by 100 and 400
+  return (
+    (year % 4 === 0 && year % 100 != 0) ||
+    (year % 100 === 0 && year % 400 === 0)
+  );
 };
 
 // Do not edit below this line
