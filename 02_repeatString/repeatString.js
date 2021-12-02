921c79f7453e7633a2d01ed1ebcced94b837f3e1
cthipsudo <chanpasong.thipphakhinkeo@gmail.com>
2021-12-01 22:22:15 -0600

    Completed JS Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    if(count < 0) return 'ERROR';
+    let newString ="";
+    while(count > 0 ) {
+        newString += string;
+        count--;
+    }
+    return newString;
 };
 
 // Do not edit below this line
