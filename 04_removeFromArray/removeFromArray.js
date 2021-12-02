301acaec0e885e9c3ca86a141bda4edd1a733a58
cthipsudo <chanpasong.thipphakhinkeo@gmail.com>
2021-12-01 23:11:59 -0600

    Completed JS Exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
 const removeFromArray = function() {
 
+    //console.log(arguments.length); length of arguments
+    let arr = arguments[0];
+
+    for(let i = 1; i < arguments.length; i++){
+        let spliceStart = arr.indexOf(arguments[i]);
+        if(spliceStart != -1) {
+            arr.splice(spliceStart, 1);
+        }
+    }
+    //console.log(arr);
+    return arr;
 };
 
 // Do not edit below this line
