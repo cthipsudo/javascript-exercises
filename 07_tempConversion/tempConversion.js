6dcd01dfa5ce1484480d3e2b2bb2230715cb85f3
cthipsudo <chanpasong.thipphakhinkeo@gmail.com>
2021-12-02 11:38:24 -0600

    Completed JS Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  // f to c is (f - 32) * (5/9)
+  return +((temp - 32) * (5/9)).toFixed(1); // toFixed returns a string, Unary + Operator converts it to number
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  // c to f is c * (9/5) + 32
+  return +(temp * (9/5) + 32).toFixed(1);
 };
 
 // Do not edit below this line
