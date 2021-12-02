1dd40844529d45cee42238e85d99059e2a1c8289
cthipsudo <chanpasong.thipphakhinkeo@gmail.com>
2021-12-01 22:38:42 -0600

    Completed JS Exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
 const reverseString = function(str) {
+    let arr = str.split('');
+    let newArr = [];
 
+    for(let i = str.length-1; i >=0; i--){
+        newArr.push(arr[i]);
+    }
+    return newArr.join('');
 };
 
 // Do not edit below this line
