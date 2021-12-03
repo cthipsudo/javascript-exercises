77015f3b8c9993f3beb7034ca991b9b28977022d
cthipsudo <chanpasong.thipphakhinkeo@gmail.com>
2021-12-03 14:53:13 -0600

    Completed JS Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const reg = /[\W]/g;
+  const newStr = str.replace(reg, '').toLowerCase();
+  let reverseStr = newStr.split('').reverse().join('');
+  console.log(reverseStr);
+  return newStr === reverseStr;
 
 };
 
