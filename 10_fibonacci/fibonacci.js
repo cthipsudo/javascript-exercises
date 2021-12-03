08217e565f2c541f8222e53445856746e30feb17
cthipsudo <chanpasong.thipphakhinkeo@gmail.com>
2021-12-03 15:20:33 -0600

    Completed JS Exercises 10 & 11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  // num is number of loops for fibonacii sequence
+  if (num < 0) {
+    return 'OOPS'
+  }
+  let a = 1;
+  let b = 0;
+  let temp;
+
+  while(num > 0) {
+    //console.log(a);
+    temp = a;
+    a = a + b;
+    b = temp;
+    num--;
+
+  }
+  return b;
 
 };
 
