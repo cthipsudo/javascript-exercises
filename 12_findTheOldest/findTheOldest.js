7d419dc8f4cc4f9541289565d0437cba0ef72677
cthipsudo <chanpasong.thipphakhinkeo@gmail.com>
2021-12-03 15:37:51 -0600

    Completed JS Exercises 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  return arr.reduce((prev, current) => {
+    let prevAge;
+    let currentYear = new Date(); 
+    if (!prev.yearOfDeath) {
+      prevAge = currentYear.getFullYear() - prev.yearOfBirth;
+    } else {
+      prevAge = prev.yearOfDeath - prev.yearOfBirth;
+    }
 
+    const currentAge = current.yearOfDeath - current.yearOfBirth;
+    return prevAge > currentAge ? prev : current;
+  });
 };
 
 // Do not edit below this line
